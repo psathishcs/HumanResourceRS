e83dfd473076f6833975181f8f5e3efe523bf7ae
DESKTOP-7D1NSHG\Sathish <p.sathish.cs@gmail.com>
2017-01-17 20:53:59 -0600

    fix

--- src/main/java/org/hr/HumanResourceApplication.java
+++ src/main/java/org/hr/HumanResourceApplication.java
@@ -3,8 +3,9 @@ package org.hr;
 import org.springframework.boot.SpringApplication;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
 import org.springframework.context.annotation.Bean;
-import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
@@ -13,6 +14,7 @@ import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableEurekaClient
 @EnableSwagger2
 public class HumanResourceApplication {
 	public static void main(String[] args){
