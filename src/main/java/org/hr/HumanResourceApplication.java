69f8ccbffb63e7048048fee669bc1e45ef93700d
Sathish Kumar Parthasarathy <p.sathish.cs@gmail.com>
2017-01-19 18:52:46 -0600

    gradle build file changes

--- src/main/java/org/hr/HumanResourceApplication.java
+++ src/main/java/org/hr/HumanResourceApplication.java
@@ -4,7 +4,6 @@ import org.springframework.boot.SpringApplication;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-
 import org.springframework.context.annotation.Bean;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
